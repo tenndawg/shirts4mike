5a1c427e103218cf6cb71cd28f5928e418214d75
TennDawg <nickt@mojomarketplace.com>
2014-10-16 14:06:26 -0600

    no message

---
+++ search/index.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once("../inc/config.php");
+
+$pageTitle = "Search";
+$section = "search";
+include(ROOT_PATH . "inc/header.php"); ?>
+
+
+
+<?php include(ROOT_PATH . "inc/footer.php"); ?>

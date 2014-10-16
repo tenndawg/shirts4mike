018280ac687afd3c56f4a409a66db1ecedbda903
TennDawg <nickt@mojomarketplace.com>
2014-10-16 15:22:10 -0600

    updated

--- search/index.php
+++ search/index.php
@@ -6,6 +6,19 @@ $pageTitle = "Search";
 $section = "search";
 include(ROOT_PATH . "inc/header.php"); ?>
 
+	<div class="section shirts search page">
 
+		<div class="wrapper">
+
+			<h1>Search</h1>
+
+			<form>
+				<input type="text" name="s">
+				<input type="submit" value="Go">
+			</form>
+
+		</div>
+
+	</div>
 
 <?php include(ROOT_PATH . "inc/footer.php"); ?>

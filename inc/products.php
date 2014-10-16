8e8f9a3868b8e5dd10366b7869a7732ec692f6db
TennDawg <nickt@mojomarketplace.com>
2014-10-16 15:50:18 -0600

    no message

--- inc/products.php
+++ inc/products.php
@@ -36,77 +36,80 @@ function get_products_search($s) {
     return $results;
 }
 
-$products = array();
-$products[101] = array(
-    "name" => "Logo Shirt, Red",
-    "img" => "img/shirts/shirt-101.jpg",
-    "price" => 18,
-    "paypal" => "9P7DLECFD4LKE",
-    "sizes" => array("Small","Medium","Large","X-Large"),
-    "styles" => array("Long Sleeve","Short Sleeve","Half Sleeve","Tank Top")
-);
-$products[102] = array(
-	  "name" => "Mike the Frog Shirt, Black",
-    "img" => "img/shirts/shirt-102.jpg",
-    "price" => 20,
-    "paypal" => "SXKPTHN2EES3J",
-    "sizes" => array("Small","Medium","Large","X-Large"),
-    "styles" => array("Long Sleeve","Short Sleeve","Half Sleeve","Tank Top")
-);
-$products[103] = array(
-    "name" => "Mike the Frog Shirt, Blue",
-    "img" => "img/shirts/shirt-103.jpg",    
-    "price" => 20,
-    "paypal" => "7T8LK5WXT5Q9J",
-    "sizes" => array("Small","Medium","Large","X-Large"),
-    "styles" => array("Long Sleeve","Short Sleeve","Half Sleeve","Tank Top")
-);
-$products[104] = array(
-    "name" => "Logo Shirt, Green",
-    "img" => "img/shirts/shirt-104.jpg",    
-    "price" => 18,
-    "paypal" => "YKVL5F87E8PCS",
-    "sizes" => array("Small","Medium","Large","X-Large"),
-    "styles" => array("Long Sleeve","Short Sleeve","Half Sleeve","Tank Top")
-);
-$products[105] = array(
-    "name" => "Mike the Frog Shirt, Yellow",
-    "img" => "img/shirts/shirt-105.jpg",    
-    "price" => 25,
-    "paypal" => "4CLP2SCVYM288",
-    "sizes" => array("Small","Medium","Large","X-Large"),
-    "styles" => array("Long Sleeve","Short Sleeve","Half Sleeve","Tank Top")
-);
-$products[106] = array(
-    "name" => "Logo Shirt, Gray",
-    "img" => "img/shirts/shirt-106.jpg",    
-    "price" => 20,
-    "paypal" => "TNAZ2RGYYJ396",
-    "sizes" => array("Small","Medium","Large","X-Large"),
-    "styles" => array("Long Sleeve","Short Sleeve","Half Sleeve","Tank Top")
-);
-$products[107] = array(
-    "name" => "Logo Shirt, Teal",
-    "img" => "img/shirts/shirt-107.jpg",    
-    "price" => 20,
-    "paypal" => "S5FMPJN6Y2C32",
-    "sizes" => array("Small","Medium","Large","X-Large"),
-    "styles" => array("Long Sleeve","Short Sleeve","Half Sleeve","Tank Top")
-);
-$products[108] = array(
-    "name" => "Mike the Frog Shirt, Orange",
-    "img" => "img/shirts/shirt-108.jpg",    
-    "price" => 25,
-    "paypal" => "JMFK7P7VEHS44",
-    "sizes" => array("Large","X-Large"),
-    "styles" => array("Long Sleeve","Short Sleeve","Half Sleeve","Tank Top")
-);
+function get_products_all() {
+    $products = array();
+    $products[101] = array(
+        "name" => "Logo Shirt, Red",
+        "img" => "img/shirts/shirt-101.jpg",
+        "price" => 18,
+        "paypal" => "9P7DLECFD4LKE",
+        "sizes" => array("Small","Medium","Large","X-Large"),
+        "styles" => array("Long Sleeve","Short Sleeve","Half Sleeve","Tank Top")
+    );
+    $products[102] = array(
+    	  "name" => "Mike the Frog Shirt, Black",
+        "img" => "img/shirts/shirt-102.jpg",
+        "price" => 20,
+        "paypal" => "SXKPTHN2EES3J",
+        "sizes" => array("Small","Medium","Large","X-Large"),
+        "styles" => array("Long Sleeve","Short Sleeve","Half Sleeve","Tank Top")
+    );
+    $products[103] = array(
+        "name" => "Mike the Frog Shirt, Blue",
+        "img" => "img/shirts/shirt-103.jpg",    
+        "price" => 20,
+        "paypal" => "7T8LK5WXT5Q9J",
+        "sizes" => array("Small","Medium","Large","X-Large"),
+        "styles" => array("Long Sleeve","Short Sleeve","Half Sleeve","Tank Top")
+    );
+    $products[104] = array(
+        "name" => "Logo Shirt, Green",
+        "img" => "img/shirts/shirt-104.jpg",    
+        "price" => 18,
+        "paypal" => "YKVL5F87E8PCS",
+        "sizes" => array("Small","Medium","Large","X-Large"),
+        "styles" => array("Long Sleeve","Short Sleeve","Half Sleeve","Tank Top")
+    );
+    $products[105] = array(
+        "name" => "Mike the Frog Shirt, Yellow",
+        "img" => "img/shirts/shirt-105.jpg",    
+        "price" => 25,
+        "paypal" => "4CLP2SCVYM288",
+        "sizes" => array("Small","Medium","Large","X-Large"),
+        "styles" => array("Long Sleeve","Short Sleeve","Half Sleeve","Tank Top")
+    );
+    $products[106] = array(
+        "name" => "Logo Shirt, Gray",
+        "img" => "img/shirts/shirt-106.jpg",    
+        "price" => 20,
+        "paypal" => "TNAZ2RGYYJ396",
+        "sizes" => array("Small","Medium","Large","X-Large"),
+        "styles" => array("Long Sleeve","Short Sleeve","Half Sleeve","Tank Top")
+    );
+    $products[107] = array(
+        "name" => "Logo Shirt, Teal",
+        "img" => "img/shirts/shirt-107.jpg",    
+        "price" => 20,
+        "paypal" => "S5FMPJN6Y2C32",
+        "sizes" => array("Small","Medium","Large","X-Large"),
+        "styles" => array("Long Sleeve","Short Sleeve","Half Sleeve","Tank Top")
+    );
+    $products[108] = array(
+        "name" => "Mike the Frog Shirt, Orange",
+        "img" => "img/shirts/shirt-108.jpg",    
+        "price" => 25,
+        "paypal" => "JMFK7P7VEHS44",
+        "sizes" => array("Large","X-Large"),
+        "styles" => array("Long Sleeve","Short Sleeve","Half Sleeve","Tank Top")
+    );
 
-foreach ($products as $product_id => $product) {
-	$products[$product_id]["sku"] = $product_id;
+    foreach ($products as $product_id => $product) {
+    	$products[$product_id]["sku"] = $product_id;
+    }
+
+    return $products;
+
 }
-
-
 
 
 ?>

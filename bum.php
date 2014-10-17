74ea7a5f7f06e477673a81d849f87f0cf459a879
TennDawg <nickt@mojomarketplace.com>
2014-10-17 14:19:59 -0600

    no message

--- bum.php
+++ bum.php
@@ -1,27 +1,3 @@
-<?php
+<pre><?php
 
-$numbers = array(1,2,3,4);
-
-$total = count($numbers);
-
-$sum = 0;
-
-$output = "";
-
-$i = 0;
-
-foreach($numbers as $number) {
-
-    $i = $i + 1;
-
-    if ($i < $total) {
-
-        $sum = $sum + $number;
-
-    }
-
-}
-
-echo $sum;
-
-?>
+echo strpos("cookie dough","oo");

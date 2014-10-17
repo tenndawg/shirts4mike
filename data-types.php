9ecad43908c5b9bdbe628d75783f31006990b109
TennDawg <nickt@mojomarketplace.com>
2014-10-17 14:38:34 -0600

    no message

---
+++ data-types.php
@@ -0,0 +1,12 @@
+<pre><?php
+
+$store = "Ye Olde Ice Cream Shoppe";
+var_dump($store);
+
+$fans = 926;
+echo "\n";
+var_dump($fans);
+
+$flavors = array("Vanilla","Cookie Dough");
+echo "\n";
+var_dump($flavors);

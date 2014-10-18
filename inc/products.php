a9e8aeab11995085f373c9b41b486e764d1e74a3
TennDawg <nickt@mojomarketplace.com>
2014-10-18 15:24:42 -0600

    no message

--- inc/products.php
+++ inc/products.php
@@ -108,13 +108,186 @@ function get_products_all() {
         "sizes" => array("Large","X-Large"),
         "styles" => array("Long Sleeve","Short Sleeve","Half Sleeve","Tank Top")
     );
+$products[109] = array(
+            "name" => "Get Coding Shirt, Gray",
+            "img" => "img/shirts/shirt-109.jpg",    
+            "price" => 20,
+            "paypal" => "B5DAJHWHDA4RC",
+            "sizes" => array("Small","Medium","Large","X-Large")
+    );
+    $products[110] = array(
+            "name" => "HTML5 Shirt, Orange",
+            "img" => "img/shirts/shirt-110.jpg",    
+            "price" => 22,
+            "paypal" => "6T2LVA8EDZR8L",
+            "sizes" => array("Small","Medium","Large","X-Large")
+    );
+    $products[111] = array(
+            "name" => "CSS3 Shirt, Gray",
+            "img" => "img/shirts/shirt-111.jpg",    
+            "price" => 22,
+            "paypal" => "MA2WQGE2KCWDS",
+            "sizes" => array("Small","Medium","Large","X-Large")
+    );
+    $products[112] = array(
+            "name" => "HTML5 Shirt, Blue",
+            "img" => "img/shirts/shirt-112.jpg",    
+            "price" => 22,
+            "paypal" => "FWR955VF5PALA",
+            "sizes" => array("Small","Medium","Large","X-Large")
+    );
+    $products[113] = array(
+            "name" => "CSS3 Shirt, Black",
+            "img" => "img/shirts/shirt-113.jpg",    
+            "price" => 22,
+            "paypal" => "4ELH2M2FW7272",
+            "sizes" => array("Small","Medium","Large","X-Large")
+    );
+    $products[114] = array(
+            "name" => "PHP Shirt, Yellow",
+            "img" => "img/shirts/shirt-114.jpg",    
+            "price" => 24,
+            "paypal" => "AT3XQ3ZVP2DZG",
+            "sizes" => array("Small","Medium","Large","X-Large")
+    );
+    $products[115] = array(
+            "name" => "PHP Shirt, Purple",
+            "img" => "img/shirts/shirt-115.jpg",    
+            "price" => 24,
+            "paypal" => "LYESEKV9JWE3A",
+            "sizes" => array("Small","Medium","Large","X-Large")
+    );
+    $products[116] = array(
+            "name" => "PHP Shirt, Green",
+            "img" => "img/shirts/shirt-116.jpg",    
+            "price" => 24,
+            "paypal" => "KT7MRRJUXZR34",
+            "sizes" => array("Small","Medium","Large","X-Large")
+    );
+    $products[117] = array(
+            "name" => "Get Coding Shirt, Red",
+            "img" => "img/shirts/shirt-117.jpg",    
+            "price" => 20,
+            "paypal" => "5UXJG8PXRXFKE",
+            "sizes" => array("Small","Medium","Large","X-Large")
+    );
+    $products[118] = array(
+            "name" => "Mike the Frog Shirt, Purple",
+            "img" => "img/shirts/shirt-118.jpg",    
+            "price" => 25,
+            "paypal" => "KHP8PYPDZZFTA",
+            "sizes" => array("Small","Medium","Large","X-Large")
+    );
+    $products[119] = array(
+            "name" => "CSS3 Shirt, Purple",
+            "img" => "img/shirts/shirt-119.jpg",    
+            "price" => 22,
+            "paypal" => "BFJRFE24L93NW",
+            "sizes" => array("Small","Medium","Large","X-Large")
+    );
+    $products[120] = array(
+            "name" => "HTML5 Shirt, Red",
+            "img" => "img/shirts/shirt-120.jpg",    
+            "price" => 22,
+            "paypal" => "RUVJSBR9FXXWQ",
+            "sizes" => array("Small","Medium","Large","X-Large")
+    );
+    $products[121] = array(
+            "name" => "Get Coding Shirt, Blue",
+            "img" => "img/shirts/shirt-121.jpg",    
+            "price" => 20,
+            "paypal" => "PGN6ULGFZTXL4",
+            "sizes" => array("Small","Medium","Large","X-Large")
+    );
+    $products[122] = array(
+            "name" => "PHP Shirt, Gray",
+            "img" => "img/shirts/shirt-122.jpg",    
+            "price" => 24,
+            "paypal" => "PYR4QH97W2TSJ",
+            "sizes" => array("Small","Medium","Large","X-Large")
+    );
+    $products[123] = array(
+            "name" => "Mike the Frog Shirt, Green",
+            "img" => "img/shirts/shirt-123.jpg",    
+            "price" => 25,
+            "paypal" => "STDAUJJTSPT54",
+            "sizes" => array("Small","Medium","Large","X-Large")
+    );
+    $products[124] = array(
+            "name" => "Logo Shirt, Yellow",
+            "img" => "img/shirts/shirt-124.jpg",    
+            "price" => 20,
+            "paypal" => "2R2U74KWU5RXG",
+            "sizes" => array("Small","Medium","Large","X-Large")
+    );
+    $products[125] = array(
+            "name" => "CSS3 Shirt, Blue",
+            "img" => "img/shirts/shirt-125.jpg",    
+            "price" => 22,
+            "paypal" => "GJG7F8EW3XFAS",
+            "sizes" => array("Small","Medium","Large","X-Large")
+    );
+    $products[126] = array(
+            "name" => "Doctype Shirt, Green",
+            "img" => "img/shirts/shirt-126.jpg",    
+            "price" => 25,
+            "paypal" => "QW2LFRYGU7L4Q",
+            "sizes" => array("Small","Medium","Large","X-Large")
+    );
+    $products[127] = array(
+            "name" => "Logo Shirt, Purple",
+            "img" => "img/shirts/shirt-127.jpg",    
+            "price" => 20,
+            "paypal" => "GFV6QVRMJU7F8",
+            "sizes" => array("Small","Medium","Large","X-Large")
+    );
+    $products[128] = array(
+            "name" => "Doctype Shirt, Purple",
+            "img" => "img/shirts/shirt-128.jpg",    
+            "price" => 25,
+            "paypal" => "BARQMHMB565PN",
+            "sizes" => array("Small","Medium","Large","X-Large")
+    );
+    $products[129] = array(
+            "name" => "Get Coding Shirt, Green",
+            "img" => "img/shirts/shirt-129.jpg",    
+            "price" => 20,
+            "paypal" => "DH9GXABU3P8GS",
+            "sizes" => array("Small","Medium","Large","X-Large")
+    );
+    $products[130] = array(
+            "name" => "HTML5 Shirt, Teal",
+            "img" => "img/shirts/shirt-130.jpg",    
+            "price" => 22,
+            "paypal" => "4LZ3EUVCBENE4",
+            "sizes" => array("Small","Medium","Large","X-Large")
+    );
+    $products[131] = array(
+            "name" => "Logo Shirt, Orange",
+            "img" => "img/shirts/shirt-131.jpg",    
+            "price" => 20,
+            "paypal" => "7BNDYJBKWD364",
+            "sizes" => array("Small","Medium","Large","X-Large")
+    );
+    $products[132] = array(
+            "name" => "Mike the Frog Shirt, Red",
+            "img" => "img/shirts/shirt-132.jpg",    
+            "price" => 25,
+            "paypal" => "Y6EQRE445MYYW",
+            "sizes" => array("Small","Medium","Large","X-Large")
+    );     
 
     foreach ($products as $product_id => $product) {
-    	$products[$product_id]["sku"] = $product_id;
+        $products[$product_id]["sku"] = $product_id;
     }
 
     return $products;
-
 }
+
+
+
+
+
+
 
 ?>

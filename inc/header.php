5104cece6b5aa81840b3556c562bb188d4ad9d81
TennDawg <nickt@mojomarketplace.com>
2014-10-18 16:33:41 -0600

    no message

--- inc/header.php
+++ inc/header.php
@@ -1,8 +1,5 @@
-<?php require_once("config.php"); ?>
-
 <html>
 <head>
-  
 	<title><?php echo $pageTitle; ?></title>
 	<link rel="stylesheet" href="<?php echo BASE_URL; ?>css/style.css" type="text/css">
 	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Oswald:400,700" type="text/css">
@@ -15,13 +12,12 @@
 		<div class="wrapper">
 
 			<h1 class="branding-title"><a href="<?php echo BASE_URL; ?>">Shirts 4 Mike</a></h1>
-					
+
 			<ul class="nav">
 				<li class="shirts <?php if ($section == "shirts") { echo "on"; } ?>"><a href="<?php echo BASE_URL; ?>shirts/">Shirts</a></li>
 				<li class="contact <?php if ($section == "contact") { echo "on"; } ?>"><a href="<?php echo BASE_URL; ?>contact/">Contact</a></li>
-				<li class="about <?php if ($section == "about") { echo "on"; } ?>"><a href="<?php echo BASE_URL; ?>about/">About</a></li>
-				<li class="search <?php if ($section == "search") { echo "on"; } ?>"><a href="<?php echo BASE_URL; ?>search/">Search</a></li>			
-				<li class="cart"><li class="cart"><a target="paypal" href="https://www.paypal.com/cgi-bin/webscr?cmd=_cart&amp;business=Q6NFNPFRBWR8S&amp;display=1">Shopping Cart</a></li>
+				<li class="search <?php if ($section == "search") { echo "on"; } ?>"><a href="<?php echo BASE_URL; ?>search/">Search</a></li>
+				<li class="cart"><a target="paypal" href="https://www.paypal.com/cgi-bin/webscr?cmd=_cart&amp;business=Q6NFNPFRBWR8S&amp;display=1">Shopping Cart</a></li>
 			</ul>
 
 		</div>

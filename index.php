c74e2155f100ad156d5bfab407a567f9893f3978
TennDawg <nickt@mojomarketplace.com>
2014-10-18 16:31:16 -0600

    no message

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <?php 
 
 require_once("inc/config.php");
+include(ROOT_PATH . "inc/products.php");
+$recent = get_products_recent();
 
 $pageTitle = "Unique T-shirts designed by a frog";
 $section = "home";
-include('inc/header.php'); ?>
+include(ROOT_PATH . 'inc/header.php'); ?>
 		<div class="section banner">
 
 			<div class="wrapper">
 
-				<img class="hero" src="img/mike-the-frog.png" alt="Mike the Frog says:">
+				<img class="hero" src="<?php echo BASE_URL; ?>img/mike-the-frog.png" alt="Mike the Frog says:">
 				<div class="button">
-					<a href="shirts.php">
+					<a href="<?php echo BASE_URL; ?>shirts.php">
 						<h2>Hey, I&rsquo;m Mike!</h2>
 						<p>Check Out My Shirts</p>
 					</a>
@@ -26,20 +28,14 @@ include('inc/header.php'); ?>
 
 				<h2>Mike&rsquo;s Latest Shirts</h2>
 
-        <?php
-		 include(ROOT_PATH . 'inc/products.php'); 
-		$recent = get_products_recent();
-	?>		
-		<ul class="products">
+				<ul class="products">
 					<?php
-
-   
-              $list_view_html = "";
-              foreach($recent as $product) { 
-                      $list_view_html = get_list_view_html($product) . $list_view_html;
-              }
-              echo $list_view_html;
-          ?>								
+						$list_view_html = "";
+						foreach($recent as $product) { 
+							$list_view_html = get_list_view_html($product) . $list_view_html;
+						}
+						echo $list_view_html;
+					?>								
 				</ul>
 
 			</div>
